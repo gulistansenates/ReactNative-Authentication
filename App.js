88c11bd4dea7926c6a273173962a97632c4a80b8
gulistansenates <95415294+gulistansenates@users.noreply.github.com>
2022-04-02 16:15:39 +0300

    fix an issue and typo

--- App.js
+++ App.js
@@ -7,22 +7,41 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StyleSheet,Text,} from 'react-native';
-import Navigation from './src/navigation';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import Navigation from './src/navigation/index';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import NewPasswordScreen from '../screens/NewPasswordScreen';
+import HomeScreen from '../screens/HomeScreen';
 
-const App  = () => {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <SafeAreaView style = {styles.root}>
-      <Navigation/>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SignIn"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  root : {
-    flex: 1 ,
-    backgroundColor: '#F9FBFC'
+  root: {
+    flex: 1,
+    backgroundColor: '#F9FBFC',
   },
 });
 
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,6 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { View, Text } from 'react-native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {View, Text} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
@@ -9,24 +9,22 @@ import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 import NewPasswordScreen from '../screens/NewPasswordScreen';
 import HomeScreen from '../screens/HomeScreen';
 
-
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-       <Stack.Navigator screenOptions={{headerShown: false}}>
-         
-          <Stack.Screen name = "SignIn" component ={SignInScreen}/>
-          <Stack.Screen name = "SignUp" component ={SignUpScreen}/>
-          <Stack.Screen name = "ConfirmEmail" component ={ConfirmEmailScreen}/>
-          <Stack.Screen name = "ForgotPassword" component ={ForgotPasswordScreen}/>
-          <Stack.Screen name = "NewPassword" component ={NewPasswordScreen}/>
-          <Stack.Screen name = "HomeScreen" component ={HomeScreen}/>
-
-       </Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName="SignIn"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-
   );
 };
 
